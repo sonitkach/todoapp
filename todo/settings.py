26abf86b84a5bfe31fc66c78945b61374a49b90b
ASUS <sonitkach@gmail.com>
2020-12-03 15:23:34 +0300

    extracted css and js into static folder

--- todo/settings.py
+++ todo/settings.py
@@ -126,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
